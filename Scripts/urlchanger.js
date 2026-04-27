4ca833e9539bbf58b5cde1c5b05c6f1995e0d125
Matthias Maes <94961568+MM-13@users.noreply.github.com>
2026-04-27 14:48:02 +0200

    reassigned links

--- Scripts/urlchanger.js
+++ Scripts/urlchanger.js
@@ -1,8 +1,8 @@
 (function () {
     const path = window.location.pathname;
-    if (path.endsWith('index.html')) {
+    /*if (path.endsWith('index.html')) {
         history.replaceState(null, '', path.replace('index.html', 'home'));
-    } else if (path.endsWith('.html')) {
+    } else*/ if (path.endsWith('.html')) {
         history.replaceState(null, '', path.slice(0, -5));
     }
 })();
